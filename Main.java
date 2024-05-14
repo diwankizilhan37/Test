024031f68f6f9d6a3703b0268d1950c673749972
diwankizilhan37 <diwan.kizilhan.social@gmx.de>
2024-05-14 11:15:00 +0200

    Zusammengesetzte Operatoren werden verwendet

--- Main.java
+++ Main.java
@@ -7,14 +7,14 @@ public class Main
         
         for(int i = 1; i <= 10; ++i)
         {
-            summe = summe + 1;
+            summe += 1;
         }
         
         int mult = 1;
         
         for(int i = 2; i <= 10; ++i)
         {
-            mult = mult * i;
+            mult *= i;
         }
     }
 }
